d75b5b8c681ca4ea936ebdf78110ce18586b1dae
Lucas <jlabbude@hotmail.com>
2024-05-16 11:01:31 -0300

    version status

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="AbeewayConfig",
-    version="0.111",
+    version="0.2",
     description="Abeeway configuration tool",
     author="João Lucas",
     url="https://github.com/jlabbude/AbeewayConfig",
