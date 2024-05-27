b3b584d942163f1928e169bf859c2ba42a484536
Lucas <jlabbude@hotmail.com>
2024-05-27 15:49:02 -0300

    changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="AbeewayConfig",
-    version="0.2.3",
+    version="0.2.5",
     description="Abeeway configuration tool",
     author="João Lucas",
     url="https://github.com/jlabbude/AbeewayConfig",
