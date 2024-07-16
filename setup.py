1c504aed3bb00cc4b4efcfa89bfbcff283e0790c
Lucas <jlabbude@hotmail.com>
2024-07-16 15:20:44 -0300

    fixed a bug when installing fuzzyfinder from requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="SerialManager",
-    version="1.9",
+    version="1.9.0.1",
     description="Abeeway configuration tool",
     author="João Lucas",
     url="https://github.com/jlabbude/SerialManager",
